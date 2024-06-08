bf22bf3fda80c0e904ac07747a21fba5fef3f592
andresalban <andresalban@unicauca.edu.co>
2024-06-07 22:37:46 -0500

    Aula 2 -Creando Componentes- terminda

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './assets/css/componentes/card.css'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Page404 from "./pages/Page404";
+import Header from "./components/Header";
 
 function App() {
 
-
     return (
         <Router>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/sobre" element={<Sobre/>}/>
+                <Route path="*" element={<Page404/>}/>
             </Routes>
         </Router>
     );
