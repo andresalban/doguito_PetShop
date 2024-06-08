36013b6f02bc913301141efbe07f8a2457fade49
andresalban <andresalban@unicauca.edu.co>
2024-06-07 20:02:39 -0500

    aula 1 -Router- terminada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
 
 function App() {
-  return (
-    <>
 
-    </>
-  );
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/sobre" element={<Sobre/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
