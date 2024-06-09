236b55c86323a838044fd55b09e333e84541c614
andresalban <andresalban@unicauca.edu.co>
2024-06-08 19:01:41 -0500

    Aula 5 -rutas Anidadas-Curso terminado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home";
-import Sobre from "./pages/Sobre";
-import Page404 from "./pages/Page404";
-import Header from "./components/Header";
-import Post from "./pages/Post";
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Page404 from './pages/Page404';
+import Header from './components/Header';
+import Post from './pages/Post';
+import Categoria from './pages/Categoria';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
     return (
         <Router>
-            <Header/>
+            <Header />
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/sobre" element={<Sobre/>}/>
-                <Route path="/posts/:id" element={<Post/>}/>
-                <Route path="*" element={<Page404/>}/>
+                <Route path='/' element={<Home />} />
+                <Route path='/sobre' element={<Sobre />} />
+                <Route path='/posts/:id' element={<Post />} />
+                <Route path="/categoria/:id/*" element={<Categoria />} />
+                <Route path='*' element={<Page404 />} />
             </Routes>
         </Router>
     );
